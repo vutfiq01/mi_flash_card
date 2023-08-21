4d16d1a3271af61eb7b167d6aaa58ebf79158270
vutfiq01 <rafiq.du.math@gmail.com>
2023-08-21 12:22:46 +0600

    isolated main from all other pages

--- lib/dice_app.dart
+++ lib/dice_app.dart
@@ -1,68 +1,68 @@
-import 'package:flutter/material.dart';
-import 'dart:math';
-import 'package:mi_flash_card/widget/custom_appbar.dart';
-import 'package:mi_flash_card/widget/navigation_drawer.dart';
+// import 'package:flutter/material.dart';
+// import 'dart:math';
+// import 'package:mi_flash_card/widget/custom_appbar.dart';
+// import 'package:mi_flash_card/widget/navigation_drawer.dart';
 
-class DicePage extends StatelessWidget {
-  const DicePage({super.key});
+// class DicePage extends StatelessWidget {
+//   const DicePage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.teal,
-      appBar: CustomAppBar(title: 'Play Dice'),
-      drawer: MyNavigationDrawer(),
-      body: BodyPage(),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Scaffold(
+//       backgroundColor: Colors.teal,
+//       appBar: CustomAppBar(title: 'Play Dice'),
+//       drawer: MyNavigationDrawer(),
+//       body: BodyPage(),
+//     );
+//   }
+// }
 
-class BodyPage extends StatefulWidget {
-  const BodyPage({super.key});
+// class BodyPage extends StatefulWidget {
+//   const BodyPage({super.key});
 
-  @override
-  State<BodyPage> createState() => _BodyPageState();
-}
+//   @override
+//   State<BodyPage> createState() => _BodyPageState();
+// }
 
-class _BodyPageState extends State<BodyPage> {
-  int leftDiceNumber = 1;
-  int rightDiceNumber = 1;
-  void randomizeDice() {
-    setState(() {
-      leftDiceNumber = Random().nextInt(6) + 1;
-      rightDiceNumber = Random().nextInt(6) + 1;
-    });
-  }
+// class _BodyPageState extends State<BodyPage> {
+//   int leftDiceNumber = 1;
+//   int rightDiceNumber = 1;
+//   void randomizeDice() {
+//     setState(() {
+//       leftDiceNumber = Random().nextInt(6) + 1;
+//       rightDiceNumber = Random().nextInt(6) + 1;
+//     });
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextButton(
-                onPressed: () {
-                  randomizeDice();
-                },
-                child: Image.asset('images/dice$leftDiceNumber.png'),
-              ),
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextButton(
-                onPressed: () {
-                  randomizeDice();
-                },
-                child: Image.asset('images/dice$rightDiceNumber.png'),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Row(
+//         children: <Widget>[
+//           Expanded(
+//             child: Padding(
+//               padding: const EdgeInsets.all(8.0),
+//               child: TextButton(
+//                 onPressed: () {
+//                   randomizeDice();
+//                 },
+//                 child: Image.asset('images/dice$leftDiceNumber.png'),
+//               ),
+//             ),
+//           ),
+//           Expanded(
+//             child: Padding(
+//               padding: const EdgeInsets.all(8.0),
+//               child: TextButton(
+//                 onPressed: () {
+//                   randomizeDice();
+//                 },
+//                 child: Image.asset('images/dice$rightDiceNumber.png'),
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
