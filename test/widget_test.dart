a7a6785af1830c2c6ab9ba748fd942f1df8d2b98
vutfiq01 <rafiq.du.math@gmail.com>
2023-08-24 17:35:04 +0600

    fixed test page argument

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:mi_flash_card/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyApp(appBarTitle: 'General Profile'));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
