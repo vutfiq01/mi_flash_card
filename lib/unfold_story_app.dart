99fb36c7d3199c7020e95e43d050e6dbedabce1d
vutfiq01 <rafiq.du.math@gmail.com>
2023-08-25 15:37:07 +0600

    added background and 2 choice buttons

--- lib/unfold_story_app.dart
+++ lib/unfold_story_app.dart
@@ -12,12 +12,92 @@ class UnfoldStoryApp extends StatelessWidget {
       backgroundColor: const Color.fromARGB(255, 0, 51, 43),
       appBar: CustomAppBar(title: appBarTitle),
       drawer: const MyNavigationDrawer(),
-      body: const SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: 10.0,
+      body: const StorySection(),
+    );
+  }
+}
+
+class StorySection extends StatefulWidget {
+  const StorySection({super.key});
+
+  @override
+  State<StorySection> createState() => _StorySectionState();
+}
+
+class _StorySectionState extends State<StorySection> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          colorFilter: ColorFilter.mode(
+            Colors.teal.shade900.withOpacity(0.25),
+            BlendMode.dstATop,
           ),
-          child: Placeholder(),
+          image: const AssetImage('images/tealspacebackground.jpg'),
+        ),
+      ),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 15.0,
+        vertical: 50.0,
+      ),
+      constraints: const BoxConstraints.expand(),
+      child: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const Expanded(
+              flex: 12,
+              child: Center(
+                child: Text(
+                  'Story will appear here........',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Poppins',
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.teal[500],
+                ),
+                child: const Text(
+                  'Choice 1',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Expanded(
+              flex: 2,
+              child: TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.teal[500],
+                ),
+                child: const Text(
+                  'Choice 2',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
