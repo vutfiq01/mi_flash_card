5f88d88f9b0b0a87888dd097d9e19a78d04c06ef
vutfiq01 <rafiq.du.math@gmail.com>
2023-09-20 22:45:47 +0600

    api json data and converting them for variables

--- lib/myWeatherApp/weatherScreens/weather_loading_screen.dart
+++ lib/myWeatherApp/weatherScreens/weather_loading_screen.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 import '../weatherServices/weather_location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class WeatherLoadingScreen extends StatefulWidget {
   const WeatherLoadingScreen({super.key});
@@ -15,6 +17,7 @@ class _WeatherLoadingScreenState extends State<WeatherLoadingScreen> {
   void initState() {
     super.initState();
     getLocation();
+    getWeatherData();
   }
 
   void getLocation() async {
@@ -22,6 +25,26 @@ class _WeatherLoadingScreenState extends State<WeatherLoadingScreen> {
     await currentLoc.getCurrentLocation();
     print(currentLoc.latitude);
     print(currentLoc.longitude);
+  }
+
+  void getWeatherData() async {
+    http.Response apiResponse = await http.get(
+      Uri.parse(
+          'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=0c425dcbc42df4f505aec969715b76f3'),
+    );
+    if (apiResponse.statusCode == 200) {
+      String apiData = apiResponse.body;
+      var decodedData = jsonDecode(apiData);
+      int weatherCondition = decodedData['weather'][0]['id'];
+      double weatherTemparature = decodedData['main']['temp'];
+      String weatherLocationName = decodedData['name'];
+
+      print(weatherLocationName);
+      print(weatherTemparature);
+      print(weatherCondition);
+    } else {
+      print(apiResponse.statusCode);
+    }
   }
 
   @override
